69b94144683f6b10f51501b78fd3b49e21bdb27b
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2022-06-14 12:15:57 +0800

    Update jd_scyl.js

--- jd_scyl.js
+++ jd_scyl.js
@@ -62,7 +62,7 @@ $.logic = async function () {
     }
     await DelShopFav()
 };
-$.run({ wait: [5000, 20000], whitelist: [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17] })
+$.run({ wait: [5000, 20000], whitelist: [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22] })
     .catch(reason => $.log(reason))
 
 async function GiveShopGift () {
